04a91642c58684161f36b33f0afc6eb842ef09f7
Andrew Stone <andrew.j.stone.1@gmail.com>
2012-06-15 23:24:49 -0400

    basic working repl with dbs and routers in context

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var repl = require('./lib/repl'),
     routerFactory = require('./lib/routerFactory'),
     databaseFactory = require('./lib/dbFactory'),
-    fs = require('fs');
+    fs = require('fs'),
+    repl = require('./lib/repl');
+
+exports.routerFactory = routerFactory;
+exports.databaseFactory = databaseFactory;
+exports.repl = repl;
 
 main(); 
 
@@ -12,15 +17,16 @@ function main() {
 }
 
 function startRepl() {
-  repl.start();
+  repl.start(exports);
 }
 
 function registerDatabases() {
   var dir = __dirname+'/lib/databases';
   var files = fs.readdirSync(dir);
   files.forEach(function(file) {
+    if (file.match(/.swp$/)) return;
     var _module  = require(dir+'/'+file);
-    databaseFactory.register(_module.name, module);
+    databaseFactory.register(_module.id, _module);
   });
 }
 
@@ -28,8 +34,9 @@ function registerRouters() {
   var dir = __dirname+'/lib/routers';
   var files = fs.readdirSync(dir);
   files.forEach(function(file) {
+    if (file.match(/.swp$/)) return;
     var _module  = require(dir+'/'+file);
-    routerFactory.register(_module.name, module);
+    routerFactory.register(_module.id, _module);
   });
 }
 
